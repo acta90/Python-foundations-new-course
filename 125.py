5928be1bf5e6e3f12915106df2ada979820e893e
Ivaylo.Lambrev <acta90@gmail.com>
2020-03-10 16:35:00 +0200

    "second commit"
    Signed-off-by: Ivaylo.Lambrev <acta90@gmail.com>

---
+++ 125.py
@@ -0,0 +1,17 @@
+"""
+There is a file which contains temperatures in Celsius or Fahrenheit, one string per line. The strings are expected to look like this: 10F of -15C.
+
+Write a Python program which asks:
+
+the user for an input value for the file location
+
+reads the file line by line
+
+converts each temperature to Fahrenheit
+
+writes the converted values in a new file
+
+The input and output values should be floating-point numbers.
+
+"""
+
