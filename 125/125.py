bb34f103f9addd60ef927442cca717e8a7859ca4
Ivaylo.Lambrev <acta90@gmail.com>
2020-03-26 10:28:23 +0200

    changed folder name from 120 to 125
    Signed-off-by: Ivaylo.Lambrev <acta90@gmail.com>

---
+++ 125/125.py
@@ -0,0 +1,20 @@
+import re
+
+
+def temperature():
+    # path C:\Users\Ivaylo.Lambrev\PycharmProjects\Python-foundations-new-course\125\input
+    directory = input("Please insert the file's directory :")
+    with open(directory, 'r') as f:
+        temp = f.readlines()
+
+    with open('output', 'w') as out:
+        for i in temp:
+            # Extracting values as float from temp list
+            a = [float(s) for s in re.findall(r'-?\d+\.?\d*', i)]
+            tf = a[1] * (9/5) + 32
+            out.write(f'{a[0]}F of {tf}C \n')
+        out.close()
+
+temperature()
+
+
